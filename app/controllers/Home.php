2d16fd34e63b11a7bf7cd44b8faf63872b1392ca
Nurul Dini Kamilah <74400133+nuruldinikamilah@users.noreply.github.com>
2024-09-03 00:13:12 +0700

    add git ignore node_modules

---
+++ app/controllers/Home.php
@@ -0,0 +1,8 @@
+<?php
+
+class Home { //kelas home akan extend ke kelas controller di core
+    public function index()
+    {
+        echo 'home/index';
+    }
+}
---
+++ app/controllers/Ngasal.php
@@ -0,0 +1,12 @@
+<?php
+
+class Ngasal {
+    public function index()
+    {
+        echo "Halo, ssss";
+    }
+    public function page() 
+    {
+        echo 'ngasal/app';
+    }
+}
